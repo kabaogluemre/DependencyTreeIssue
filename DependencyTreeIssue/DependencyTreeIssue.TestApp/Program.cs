9faa9b7c1d2fbaba8dea5322e7ef5e47d57e03ba
Emre Kabaoglu <Emre.Kabaoglu@AuthorityPartners.com>
2024-03-12 11:00:45 +0300

    Initial commit

---
+++ DependencyTreeIssue/DependencyTreeIssue.TestApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace DependencyTreeIssue.TestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var serializedObject = JsonConvert.SerializeObject(new { Text = "Hello World!" });
+            Console.WriteLine(serializedObject);
+            Console.ReadLine();
+        }
+    }
+}
